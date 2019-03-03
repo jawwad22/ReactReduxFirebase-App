a71d016d9a721894be012d5a11915114d5967117
unknown <mjawwadiqbal22@gmail.com>
2019-03-03 01:00:06 -0800

    Navbar Continue

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar=()=>{
+    return(
+        <nav className="nav wrapper grey darken-3">
+            <div className="container">
+            <Link to='/' className="brand-logo">Jawwad Plan</Link>
+            </div>
+        </nav>
+    )
+}
