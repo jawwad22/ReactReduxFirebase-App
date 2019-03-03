50c5f26a0108e0153663cfa4de1283f68a8b0d13
unknown <mjawwadiqbal22@gmail.com>
2019-03-03 15:54:21 -0800

    --Dashboard Component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Titles
+                </span>
+                    <p>Posted by Jawwad</p>
+                    <p className="grey-text">
+                        3rd Speteber,2am
+                </p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Titles
+                </span>
+                    <p>Posted by Jawwad</p>
+                    <p className="grey-text">
+                        3rd Speteber,2am
+                </p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Titles
+                </span>
+                    <p>Posted by Jawwad</p>
+                    <p className="grey-text">
+                        3rd Speteber,2am
+                </p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ProjectList
