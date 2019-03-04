f2d727b503506d1daeff00656ac455c3792e5232
unknown <mjawwadiqbal22@gmail.com>
2019-03-03 16:12:20 -0800

    --Project Summary  Component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,41 +1,12 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummery'
 
 const ProjectList = () => {
     return (
         <div className="project list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Titles
-                </span>
-                    <p>Posted by Jawwad</p>
-                    <p className="grey-text">
-                        3rd Speteber,2am
-                </p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Titles
-                </span>
-                    <p>Posted by Jawwad</p>
-                    <p className="grey-text">
-                        3rd Speteber,2am
-                </p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Titles
-                </span>
-                    <p>Posted by Jawwad</p>
-                    <p className="grey-text">
-                        3rd Speteber,2am
-                </p>
-                </div>
-            </div>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
 
 
         </div>
---
+++ src/components/projects/ProjectSummery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">
+                    Project Titles
+                </span>
+                <p>Posted by Jawwad</p>
+                <p className="grey-text">
+                    3rd Speteber,2am
+                </p>
+            </div>
+        </div>
+    )
+}
+export default ProjectSummary
