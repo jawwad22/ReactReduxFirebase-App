d09dd0ee5d542bcf533095f4814ed18cf9782090
unknown <mjawwadiqbal22@gmail.com>
2019-03-05 22:42:57 -0800

    --Adding Dummy Data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,18 @@
-const initState={}
+const initState = {
+    projects: [
+        {
+            id: '1', title: 'help me find peach', content: 'blah blah blah'
+        }, {
+            id: '1', title: 'help me find peach', content: 'blah blah blah'
 
-const projectReducer=(state=initState,action)=>{
-return state
+        }, {
+            id: '1', title: 'help me find peach', content: 'blah blah blah'
+        }
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state
 }
 
 export default projectReducer
