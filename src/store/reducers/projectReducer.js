bd426985a4fd94c93c5574a1577513771f83b9b9
unknown <mjawwadiqbal22@gmail.com>
2019-03-05 23:10:45 -0800

    --using Thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -12,6 +12,11 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+ switch (action.type) {
+     case 'CREATE_PROJECT':
+         console.log('created project',action.project)
+         break;
+ }
     return state
 }
 
