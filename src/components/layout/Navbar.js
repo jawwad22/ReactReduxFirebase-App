5a6e0952e915b3182ed1c6c20a172b565f84c14e
unknown <mjawwadiqbal22@gmail.com>
2019-03-12 23:24:26 -0700

    Navbar COndition

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,20 +5,22 @@ import SignedOutLinks from './SignedOutLinks'
 import {connect} from 'react-redux'
 
 
-const Navbar=()=>{
+const Navbar=(props)=>{
+    const {auth} =props
+  const links=auth.uid? <SignedInLinks/>:<SignedOutLinks/>
     return(
         <nav className="nav wrapper grey darken-3">
             <div className="container">
             <Link to='/' className="brand-logo">Jawwad Plan</Link>
-           <SignedInLinks/>
-           <SignedOutLinks/>
+           {links}
             </div>
         </nav>
     )
 }
 const mapStateToProps=(state)=>{
-    console.log(state)
-    return {}
+    return {
+        auth:state.firebase.auth
+    }
 }
 
 export default connect(mapStateToProps)(Navbar)
